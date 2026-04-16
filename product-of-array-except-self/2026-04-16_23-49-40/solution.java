e0d67d12f40e61ec547c88be27a906113eecc3b8
Akash Paul <92991950+akashpaul092@users.noreply.github.com>
2026-04-16 23:49:41 +0530

    Add product-of-array-except-self solution at 2026-04-16_23-49-40

---
+++ product-of-array-except-self/2026-04-16_23-49-40/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        if(n == 0) return ans;
+        int prefix = 1;
+        for(int i=0; i< n; i++){
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
+        int sufix = 1;
+        for(int i=n-1; i>=0; i--){
+            ans[i] *= sufix;
+            sufix *= nums[i];
+        }
+        return ans;
+    }
+}

4d51a76be11d78ca3463b6f77bf7c3909297ed80
Akash Paul <92991950+akashpaul092@users.noreply.github.com>
2026-07-30 00:58:53 +0530

    Add palindromic-substrings solution at 2026-07-30_00-58-52

---
+++ palindromic-substrings/2026-07-30_00-58-52/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1;j<=s.length();j++){
+                if(isPalindrom(s.substring(i, j))) count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isPalindrom(String s){
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
